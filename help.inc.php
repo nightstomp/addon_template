cd0f834109530998cf2cc6f71be5f69f5fbf98dc
jeffe <jeffe@ab96fc0c-6e79-4a39-9b02-f4518f2608bb>
2010-07-13 12:01:22 +0000

    standardisiert: help und changelog include
    git-svn-id: svn://svn.rexdev.de/addons/addon_template/addon_template/trunk/addon_template@33 ab96fc0c-6e79-4a39-9b02-f4518f2608bb

--- help.inc.php
+++ help.inc.php
@@ -7,13 +7,43 @@
 *
 * @package redaxo4
 * @version 1.0
-* $Id$: 
+* $Id$:
 */
 
-// INCLUDE
+// ADDON IDENTIFIER & ROOT DIR
 ////////////////////////////////////////////////////////////////////////////////
-require_once 'config.inc.php';
+$myself = rex_request('addonname','string');
+$myroot = $REX['INCLUDE_PATH'].'/addons/'.$myself.'/';
 
-header('Location: index.php?page='.$myself.'&subpage=help');
+// LOCAL INCLUDES
+////////////////////////////////////////////////////////////////////////////////
+require_once $myroot.'/functions/function.rexdev_incparse.inc.php';
+
+// HELP CONTENT
+////////////////////////////////////////////////////////////////////////////////
+$help_includes = array
+(
+  'Hilfe'     => array('_readme.txt','textile'),
+  'Changelog' => array('_changelog.txt','textile')
+);
+
+// MAIN
+////////////////////////////////////////////////////////////////////////////////
+foreach($help_includes as $k => $v)
+{
+  if(file_exists($myroot.$v[0]))
+  {
+    echo '
+    <div class="rex-addon-output" style="overflow:auto">
+      <h2 class="rex-hl2" style="font-size:1em">'.$k.' <span style="color: gray; font-style: normal; font-weight: normal;">( '.$v[0].' )</span></h2>
+      <div class="rex-addon-content">
+        <div class="'.$myself.'">
+          '.rexdev_incparse($myroot,$v[0],$v[1],true).'
+        </div>
+      </div>
+    </div>';
+  }
+}
+
 
 ?>
