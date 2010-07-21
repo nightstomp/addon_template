f043f7856476846f282c6bd91f2c64556f5749f6
j3ff3 <j3ff3@ab96fc0c-6e79-4a39-9b02-f4518f2608bb>
2010-07-21 02:17:35 +0000

    repository desaster recovery.. ,(
    git-svn-id: svn://svn.rexdev.de/addons/addon_template/addon_template/trunk/addon_template@57 ab96fc0c-6e79-4a39-9b02-f4518f2608bb

---
+++ modules/a720_demo.modul.in.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Addon_Template - DEMO MODUL IN
+ * ### UID:m720demo ###
+ *
+ * @package redaxo4.2
+ * @version Addon 1.1
+ * @version Modul 1.0
+ * @version svn:$Id$
+ */
+
+//MODULEINGABE
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+?>
